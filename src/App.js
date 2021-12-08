471398d77fe06c681ad32e5bcbef2db8d783bffa
Shado2107 <pd39almeida@outlook.com>
2021-12-08 12:07:35 +0000

    update

--- src/App.js
+++ src/App.js
@@ -9,13 +9,22 @@ const APP_KEY = 'fd16bd4bf34d3bb88e88f88682e2bf36';
 class App extends React.Component {
 
   state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined,
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      error: false
   }
+
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
 
   getWeather = async (e) => {
     e.preventDefault();
@@ -24,25 +33,32 @@ class App extends React.Component {
 
     console.log(city);
 
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APP_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APP_KEY}`);
     const data = await api_call.json();
 
     if (city && country && data.cod !== 404){
+ 
         this.setState({
-          temperature: data.main.temp,
-          city: data.name,
-          humidity: data.sys.country,
+          city: `${data.name}, ${data.sys.country}`,
+          country: data.sys.country,
+          main: data.weather[0].main,
+          celsius: this.calCelsius(data.main.temp),
+          temp_max: this.calCelsius(data.main.temp_max),
+          temp_min: this.calCelsius(data.main.temp_min),
           description: data.weather[0].description,
           error: ''
         });
+
     } else {
       this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: 'Please enter the correct values',
+          city: undefined,
+          country: undefined,
+          main: undefined,
+          celsius: undefined,
+          temp_max: undefined,
+          temp_min: undefined,
+          description: undefined,
+          error: 'Please enter the correct values',
       })
     }
   }
@@ -60,15 +76,16 @@ class App extends React.Component {
                          <div className="col-6 title-container">
                              <Form getWeather={this.getWeather} />
                              <Weather
-                              temperature={this.state.temperature}
+                              celsius={this.state.celsius}
+                              main={this.main}
                               city={this.state.city}
                               country={this.state.country}
-                              humidity={this.state.humidity}
+                              temp_max={this.state.temp_max}
+                              temp_min={this.state.temp_min}
                               description={this.state.description}
                               error={this.state.error}
                              />
                          </div>
-   
                      </div>
                  </div>
              </div>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,23 +4,26 @@ const Weather = props => (
 
         <div className="weather__info">
             {
-                props.city && props.country && <p className="weather__key">LOcation:
-                    <span className="weather__value">{props.city}, {props.country} </span>
+                props.city && <p className="weather__key">Location:
+                    <span className="weather__value">{props.city} </span>
                 </p>
             } 
             {
-                props.temperature && <p className="weather__key">Temperature:
-                    <span className="weather__value">{props.temperature} </span>
+                props.celsius && <p className="weather__key">Celsius:
+                    <span className="weather__value">{props.celsius} C</span>
                 </p>
             } 
             {
-                props.humidity && <p className="weather__key">Humidity:
-                    <span className="weather__value">{props.humidity} </span>
+                props.description && <p className="weather__key">Main:
+                    <span className="weather__value"> {props.description.charAt(0).toUpperCase() +
+                        props.description.slice(1)} 
+                    </span>
                 </p>
-            } 
+            }
             {
-                props.description && <p className="weather__key">Conditions:
-                    <span className="weather__value">{props.description} </span>
+                 props.temp_min  &&  props.temp_max && <p className="weather__key">Main:
+                    <span className="weather__value"> {maxminTemp(props.temp_min, props.temp_max)}
+                    </span>
                 </p>
             } 
             {
@@ -33,3 +36,14 @@ const Weather = props => (
 
 
 export default Weather;
+
+function maxminTemp(min, max) {
+    if (max && min) {
+      return (
+        <h3>
+          <span className="px-4">{min}&deg;</span>
+          <span className="px-4">{max}&deg;</span>
+        </h3>
+      );
+    }
+}
